f3a38e03f177746639d8b1b5d856a69a65d114c4
dorenwick <dorenwick@gmail.com>
2024-09-22 13:26:28 +1200

    working cloud dataset reinforcement fine tuning

--- NGRAM_BUILDER/BothProgramsAtOnce.py
+++ NGRAM_BUILDER/BothProgramsAtOnce.py
@@ -18,39 +18,3 @@ if __name__ == '__main__':
     p3.join()
 
 
-
-# python BothProgramsAtOnce.py
-
-#
-# (cite_grabber) root@C.12549921:/$ python BothProgramsAtOnce.py
-# CUDA available: True
-# CUDA device count: 2
-# CUDA device name: NVIDIA GeForce RTX 4090
-# Using device: cuda
-# Starting embedding generation...
-# Processing files:   0%|                                                                                                                                                                                                                                                      | 0/2587 [00:00<?, ?it/s]
-# CUDA available: True
-# CUDA available: True
-# CUDA device count: 2
-# CUDA device name: NVIDIA GeForce RTX 4090
-# Using device: cuda:1
-# CUDA device count: 2
-# CUDA device name: NVIDIA GeForce RTX 4090
-# num_gpus:  2
-# Using 2 GPU(s): ['cuda:0', 'cuda:1']
-# /root/miniconda3/envs/cite_grabber/lib/python3.10/site-packages/huggingface_hub/file_download.py:1150: FutureWarning: `resume_download` is deprecated and will be removed in version 1.0.0. Downloads always resume when possible. If you want to force a new download, use `force_download=True`.
-#   warnings.warn(
-# /root/miniconda3/envs/cite_grabber/lib/python3.10/site-packages/huggingface_hub/file_download.py:1150: FutureWarning: `resume_download` is deprecated and will be removed in version 1.0.0. Downloads always resume when possible. If you want to force a new download, use `force_download=True`.
-#   warnings.warn(
-# Starting keyword extraction...
-# Processing files for keyword extraction:   0%|                                                                                                                                                                                                                               | 0/2587 [00:00<?, ?it/s]
-# self.field_int_map:  {'id2label': {0: 'Economics, Econometrics and Finance', 1: 'Materials Science', 2: 'Environmental Science', 3: 'Medicine', 4: 'Psychology', 5: 'Dentistry', 6: 'Business, Management and Accounting', 7: 'Engineering', 8: 'Biochemistry, Genetics and Molecular Biology', 9: 'Agricultural and Biological Sciences', 10: 'Energy', 11: 'Earth and Planetary Sciences', 12: 'Health Professions', 13: 'Chemistry', 14: 'Chemical Engineering', 15: 'Social Sciences', 16: 'Pharmacology, Toxicology and Pharmaceutics', 17: 'Arts and Humanities', 18: 'Mathematics', 19: 'Immunology and Microbiology', 20: 'Veterinary', 21: 'Decision Sciences', 22: 'Nursing', 23: 'Physics and Astronomy', 24: 'Neuroscience', 25: 'Computer Science'}, 'label2id': {'Economics, Econometrics and Finance': 0, 'Materials Science': 1, 'Environmental Science': 2, 'Medicine': 3, 'Psychology': 4, 'Dentistry': 5, 'Business, Management and Accounting': 6, 'Engineering': 7, 'Biochemistry, Genetics and Molecular Biology': 8, 'Agricultural and Biological Sciences': 9, 'Energy': 10, 'Earth and Planetary Sciences': 11, 'Health Professions': 12, 'Chemistry': 13, 'Chemical Engineering': 14, 'Social Sciences': 15, 'Pharmacology, Toxicology and Pharmaceutics': 16, 'Arts and Humanities': 17, 'Mathematics': 18, 'Immunology and Microbiology': 19, 'Veterinary': 20, 'Decision Sciences': 21, 'Nursing': 22, 'Physics and Astronomy': 23, 'Neuroscience': 24, 'Computer Science': 25}}
-# Checking for missing parquet files and verifying row counts...
-# All batch files from 0 to 2586 are present.
-#
-# Starting file processing...
-# Processing files:   0%|                                                                                                                                                                                                                                                      | 0/2587 [00:00<?, ?it/s]
-# Batches:   4%|_______
-# #
-#
-#
--- REINFORCEMENT_FINETUNING/CloudDatasetReinforcementFineTuning.py
+++ REINFORCEMENT_FINETUNING/CloudDatasetReinforcementFineTuning.py
@@ -1,52 +1,86 @@
 import faiss
 import numpy as np
 import polars as pl
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, losses
 from tqdm import tqdm
 from pymongo import MongoClient
+import torch
+from datasets import Dataset
 
 class CloudDatasetReinforcementFinetuning:
-    """
-    We want to search for works with particular title string length and author counts, and test for those. So, test on results with a single author, 2 authors, 3-10 authors, and 10+ authors.
-
-    TODO: in addition to this class, we want a class that goes through every single work, and if they are not found in k20 nn search, we add the work_id to a list of all "not found" work_id's.
-     In fact, i'd like you to create this class for us now. Here is how it will work. First, we are given an index, and we do the following queries on it:
-     1 f"{title} {field}", 2 f"{trigram} {field} {subfield}", 3 f"{' '.join(author_names[:2])} {field}", 4 f"{author_names[0] if author_names else ''} {field} {subfield}", 5 full_query
-
-     Now, we shall generate a table, or rather a parquet file, and for each one of these queries that doesn't result in the work_id being found in the top 20 nearest neighbours,
-     we will record the work_id and the results as (0, 1) in each column (named after query type). We want columns for each query string of each query type as well,
-      so we keep those query strings there for us. where 0 is not found, and 1 is found. We shall also put cited_by_count in another column, as we want that information to be made available.
-       Then we will save the works as "missed_data.parquet".
-
-    TODO: The next step shall be to generate a dataset of triplets (anchor, positive, negative) from the query_strings that didn't result in the work_id being found.
-        To do this, we will create a vectordb of all of them, by encoding them, and then searching via each query_string and then making positive/negatives from them,
-         (making sure there is reasonable distance between them all), until we have got every query_string built placed into triplets.
-         This will require some effort and planning to figure out the specifics.
-
-          TODO The next step shall be to fine-tune the encoder that was used to make the previous database, by incorporating the triplets into the last dataset (by adding them in),
-            then shuffling the dataset, and training the model again. We may also want to remove data from the dataset where the knn got recall@1 100% score on every query_string. We could filter them out.
-            This is something to figure out later on.
-
-    TODO: The next step is to build the vectordb with new encodings and index it all over again. Consider This description.
-        We have a class that
-
-
-
-
 
     """
+    1: The goal of this class is to create a training system for building datasets that will fine-tune the perfect model.
+
+    TODO: To accomplish this, we will do the following. We will take an input:
+        model
+        model_directory
+        faiss_index
+        faiss_index_directory
+        triplets.parquet
+                triplets_directory
+        database (such as mongodb), which stores the document string data. all database details here as an input.
+        model_output_directory
+        faiss_index_output_directory
+        triplets_output_directory
+
+        Provide a schema of query string types where query strings are built from work object fields in our mongodb database.
+        The schema (and method) contains instructions on how to build particular query string types from the fields of the work object.
+
+        1 We use faiss knn search through our index to find which works are found in the top k nearest neighbors, and then
+        2 Create a dataframe and parquet file of all the works that were not found in the top k nearest neighbors, with query string types and reuslts from mongodb
+         or our structured database as well as all the query types.
+        3 Then, we take all of the query strings that didn't result in the search result, and we take the full_string of the work object,
+        and encode them into encodings (using matryoshka, with adaptive layers).
+        4 Then, we index them, and do hard negative triplet mining to create triplets.
+        5 Then, we mix the triplets into our current triplet dataset, shuffle the new dataset of triplets we have made.
+        6 Then, we fine-tune our previous encoder on the new dataset of triplets.
+        7 Then, we use the fine-tuned encoder to create embeddings for each document in our database (mongodb, on document strings).
+        8 Then, we index the new embeddings into a new index via faiss.
+
+        At this point, we have completed one epoch and are ready to begin steps 1-8 again.
+        The goal here is to construct a system for repeating epochs 1 through 8.
+        A stopping point should be when the recall@20 scores aren't changing much between epochs.
+
+        So, to build this class, we note some specific details:
+            faiss index parameters need to be given to us, and by default will be calculated for us
+            sentence encoder parameters need to be given to us (default is 32 dimension)
+
+        So, we will show you our faiss index builder code.
+            we will show you our sentence transformer builder code.
+            we will show you our database code.
+
+        More importantly, is the fact that we need to have a directory schema setup so that we create a new directory
+        for each epoch, we we have a epoch_# output directory that contains output directories for index, triplets, and model.
+        Since this will use a ton of space, we may want a system set up to keep only the best index and best model, and best dataset.
+        That will be something to account for.
+        But lets not make that the default system. For now the default will be to save everything to disk for each epoch.
+
+        Currently, our class has a system built for building a missed_data.parquet file and for training a sentence encoder.
+        I want you to provide extra steps 1-8, and the directory builder outline as well.
 
 
-    def __init__(self, index_path, model_path, mongodb_url, database_name):
+
+
+
+
+
+    """
+
+
+    def __init__(self, index_path, model_path, mongodb_url, database_name, output_directory):
         self.index_path = index_path
         self.model_path = model_path
         self.mongodb_url = mongodb_url
         self.database_name = database_name
+        self.output_directory = output_directory
         self.index = None
         self.model = None
         self.mongo_client = None
         self.db = None
         self.works_collection = None
+        self.matryoshka_dims = [384, 256, 128, 64, 32]
+        self.use_adaptive_layers = True
 
     def load_resources(self):
         print("Loading index...")
@@ -59,6 +93,7 @@ class CloudDatasetReinforcementFinetuning:
         self.mongo_client = MongoClient(self.mongodb_url)
         self.db = self.mongo_client[self.database_name]
         self.works_collection = self.db['Works']
+
 
     def generate_queries(self, work):
         title = work.get('display_name', '')
@@ -165,11 +200,64 @@ class CloudDatasetReinforcementFinetuning:
 
         return missed_df
 
+    def create_matryoshka_embeddings(self, missed_df):
+        print("Creating Matryoshka embeddings...")
 
+        # Combine all query strings into a single list
+        all_queries = missed_df['query_1'].tolist() + missed_df['query_2'].tolist()
+
+        # Create Matryoshka loss
+        base_loss = losses.MultipleNegativesRankingLoss(model=self.model)
+        if self.use_adaptive_layers:
+            loss = losses.Matryoshka2dLoss(
+                model=self.model,
+                loss=base_loss,
+                matryoshka_dims=self.matryoshka_dims,
+                n_layers_per_step=1,
+                n_dims_per_step=1
+            )
+        else:
+            loss = losses.MatryoshkaLoss(
+                model=self.model,
+                loss=base_loss,
+                matryoshka_dims=self.matryoshka_dims
+            )
+
+        # Fine-tune the model with Matryoshka loss
+        train_dataset = Dataset.from_dict({'text': all_queries})
+        self.model.fit(
+            train_objectives=[(train_dataset, loss)],
+            epochs=1,
+            warmup_steps=100,
+            output_path=f"{self.output_directory}/matryoshka_model"
+        )
+
+        # Generate 32-dimensional embeddings
+        embeddings = self.model.encode(all_queries, convert_to_tensor=True)[:, :32]
+        return embeddings.cpu().numpy()
+
+    def create_vectordb(self, embeddings):
+        print("Creating vector database...")
+        dimension = embeddings.shape[1]  # Should be 32
+        index = faiss.IndexFlatL2(dimension)
+        index.add(embeddings)
+        return index
 
     def run(self):
         self.load_resources()
-        self.find_missed_works()
+        missed_df = self.find_missed_works()
+
+        # Clear CUDA cache if using CUDA
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
+        embeddings = self.create_matryoshka_embeddings(missed_df)
+        vectordb = self.create_vectordb(embeddings)
+
+        # Save the new index
+        faiss.write_index(vectordb, f"{self.output_directory}/matryoshka_index.bin")
+        print(f"Saved new Matryoshka index to {self.output_directory}/matryoshka_index.bin")
+
 
 # Usage
 if __name__ == "__main__":
@@ -177,6 +265,7 @@ if __name__ == "__main__":
     mongodb_url = 'mongodb://localhost:27017'
     database_name = 'OpenAlex'
     model_path = r"E:\HugeDatasetBackup\DATA_CITATION_GRABBER\models\best_model"
+    output_directory = r"E:\HugeDatasetBackup\cloud_models\matryoshka_model"
 
-    finetuner = CloudDatasetReinforcementFinetuning(index_path, model_path, mongodb_url, database_name)
+    finetuner = CloudDatasetReinforcementFinetuning(index_path, model_path, mongodb_url, database_name, output_directory)
     finetuner.run()
